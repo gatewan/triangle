50600b178c1fa22b42fdddd08eb7198f10db739d
bundet <gatewawan@ymail.com>
2021-10-13 13:46:49 +0700

    pythagoras triangle validation

--- TriangleApp/TriangleAppTests/TriangleAppTests.swift
+++ TriangleApp/TriangleAppTests/TriangleAppTests.swift
@@ -10,6 +10,10 @@ import XCTest
 
 class TriangleAppTests: XCTestCase {
   
+  func testDetectPythagorasTriangle() {
+    XCTAssertEqual(try detectTriangle(6, 8, 10), "Segitiga Siku-Siku")
+  }
+  
   func testInvalidInputDetectTriangle() {
     XCTAssertThrowsError(try detectTriangle(-1, -2, -3)) { error in
       XCTAssertEqual(error as? TriangleError, TriangleError.invalidInput)
@@ -28,7 +32,7 @@ class TriangleAppTests: XCTestCase {
   }
   
   func testDetectRandomTriangle() {
-    XCTAssertEqual(try detectTriangle(3, 4, 5), "Segitiga Sembarang")
+    XCTAssertEqual(try detectTriangle(3, 5, 7), "Segitiga Sembarang")
   }
   
   func testInequalityTriangle() {
@@ -55,6 +59,8 @@ class TriangleAppTests: XCTestCase {
       return "Segitiga Sama Sisi"
     } else if sides[0] == sides[1] || sides[1] == sides[2] {
       return "Segitiga Sama Kaki"
+    } else if Double((sides[0] * sides[0] + sides[1] * sides[1])).squareRoot() == Double(sides[2]) {
+      return "Segitiga Siku-Siku"
     } else {
       return "Segitiga Sembarang"
     }
