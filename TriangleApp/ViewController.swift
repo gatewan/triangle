4bec38d164af11730335fc365169dd111027439e
bundet <gatewawan@ymail.com>
2021-10-13 14:32:31 +0700

    re-arrange project structure

---
+++ TriangleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriangleApp
+//
+//  Created by Wawan Beneran on 13/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
